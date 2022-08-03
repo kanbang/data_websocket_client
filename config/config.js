02ec1249567659b8234ecf058993967e225fc11a
limengxuan <longyue188@gmail.com>
2022-08-04 00:15:19 +0800

    config注释

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
 var conf = 
 {
-    "WSMarketConfig": {         // 配置总名称 不用动
-        "binance-f_1": {        // 交易所名称 命名规则：交易所小写-衍生品类型小写_分区id
+    "WSMarketConfig": 
+    {                           // 配置总名称 不用动
+        "binance-f_1":          // 交易所线程  命名规则：交易所小写-衍生品类型小写_分区id
+        {  
             "isstart": "1",     // 是否启动  1启动 0不启动
             "wsclass": "okx-f", // 解析类名 命名规则：交易所小写-衍生品类型小写
-            "depth": {          // 订阅数据类型 depth和trade两种
+            "depth": 
+            {                   // 订阅数据类型 depth和trade两种
                 "type": "full", // depth数据类型 目前只有full
                 "level": "20",  // depth数据档位 
                 "symbol": [     // 品种列表
@@ -12,13 +15,37 @@ var conf =
                     "ETH-USDT"
                 ]
             },
-            "trade": {          // 订阅数据类型 depth和trade两种
+            "trade": 
+            {                   // 订阅数据类型 depth和trade两种
                 "symbol": [
                     "BTC-USDT",
                     "ETH-USDT"
                 ]
             },
             "zmqstr": "tcp://127.0.0.1:5551"  //zmq发布端口
+        },
+        "binance-f_2":         // 交易所线程 命名规则：交易所小写-衍生品类型小写_分区id
+        {
+            "wsclass":"binance-f",
+            "depth":
+            {
+                "type":"limit",
+                "level":"20",
+                "symbol":
+                [
+                    "LTC-USDT",
+                    "EOS-USDT"
+                ]
+            },
+            "trade":
+            {
+                "symbol":
+                [
+                    "LTC-USDT",
+                    "EOS-USDT"
+                ]
+            },          
+            "zmqstr":"tcp://127.0.0.1:5552"
         }
     }
 }
